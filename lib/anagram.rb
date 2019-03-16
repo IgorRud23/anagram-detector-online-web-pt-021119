c7dd532496612b090466eaf38e98ccb9973cc03f
Igor Rud <rud.igor23@gmail.com>
2019-03-16 01:13:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+def initialize(word)
+  @word = word
+end
+
+def match(name)
+name.each do |words|
+  one = words.split("").sort
+  two = @word.split("").sort
+  /one/.match(two)
+end
+end
+
+
+end
